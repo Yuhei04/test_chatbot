8c622c372388a1a10f4e379bc7280d70484e17e3
Takeda-y <92105065+yuhei-y@users.noreply.github.com>
2024-07-21 15:19:26 +0900

    Initial commit

--- app.py
+++ app.py
@@ -1,28 +1,19 @@
 from flask import Flask, request, abort
+import os
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import os
 
 app = Flask(__name__)
 
-# 環境変数からLINEチャネルのアクセストークンとシークレットを取得
-LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
-LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET')
-
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(LINE_CHANNEL_SECRET)
+line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # リクエストヘッダーから署名検証のための値を取得
     signature = request.headers['X-Line-Signature']
-
-    # リクエストボディを取得
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
 
-    # 署名の検証とハンドラーへの通知
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -32,15 +23,12 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # 受け取ったメッセージ内容を取得
-    incoming_msg = event.message.text
-
-    # 応答メッセージの作成
-    reply_msg = TextSendMessage(text=f"You said: {incoming_msg}")
-
-    # 応答メッセージの送信
-    line_bot_api.reply_message(event.reply_token, reply_msg)
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='Message received')
+    )
 
 if __name__ == "__main__":
-    app.run()
-
+    # Get the port from the environment variable, default to 8080
+    port = int(os.getenv('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=True)
